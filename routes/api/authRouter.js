51a71052fa511d32724fb07a800058ec720a99f9
Oleksandr <115045030+Oleksandr-Kharkiv@users.noreply.github.com>
2023-08-07 18:09:27 +0200

    add updateAvatarUser

--- routes/api/authRouter.js
+++ routes/api/authRouter.js
@@ -3,20 +3,22 @@ import authController from "../../controllers/authController.js";
 import validateBody from "../../decorators/validateBody.js";
 import usersSchemas from "../../schemas/usersSchemas.js";
 import authenticate from "../../middlewars/authenticate.js";
-
+import upload from "../../middlewars/upload.js";
 
 const authRouter = express.Router();
 
-authRouter.post("/register", validateBody(usersSchemas.userSchema), authController.signup)
+authRouter.post("/register", upload.single("avatarURL"), validateBody(usersSchemas.userSchema), authController.signup);
 
-authRouter.post("/login", validateBody(usersSchemas.userSchema), authController.signin)
+authRouter.post("/login", validateBody(usersSchemas.userSchema), authController.signin);
 
-authRouter.get("/current", authenticate, authController.getCurrent)
+authRouter.get("/current", authenticate, authController.getCurrent);
 
 authRouter.post("/logout", authenticate, authController.signout)
 
 authRouter.patch("/", authenticate, 
 validateBody(usersSchemas.userSubscriptionSchema), 
-authController.updateSubscriptionUser)
+authController.updateSubscriptionUser);
+
+authRouter.patch("/avatars", authenticate, upload.single("avatarURL"), authController.updateAvatarUser);
 
 export default authRouter;
